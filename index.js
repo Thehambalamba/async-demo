80c2e3bf4bed63526f84e2c2ed8067c08a24e5d0
Nikola Pervic <nikolapervic@me.com>
2018-07-26 01:53:36 +0200

    Async await aproach
    Refactored the promise based aproach to use async await.

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-// Promises aproach
+// Async and await aproach
 console.log('Before');
-getUser(1)
-.then(user => getRepoitories(user.gitHubUsername))
-.then(repos => getCommits(repos[0]))
-.then(commits => console.log('Commits', commits))
-.catch(err => console.log('Error', err.message));
+
+async function displayCommits() {
+  try {
+    const user = await getUser(1);
+    const repos = await getRepoitories(user.gitHubUsername);
+    const commits = await getCommits(repos[0]);
+    console.log(commits);
+  } catch (err) {
+    console.log('Error', err.message);
+  }
+}
+displayCommits();
+
+console.log('After');
 
 function getUser(id) {
   return new Promise ((resolve, reject) => {
@@ -16,22 +25,22 @@ function getUser(id) {
   });
   
 }
-console.log('After');
+
+function getRepoitories(username) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Reading users repositories from github...');
+      // resolve(['repo1', 'repo2', 'repo3']);
+      reject(new Error('Could not get the repos.'));
+    }, 2000);
+  });
+}
 
 function getCommits(repo) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       console.log('Reading commits from the repository...');
       resolve(['commit1', 'commit2']);
-    }, 2000);
-  });
-}
-
-function getRepoitories(username) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log('Reading users repositories from github...');
-      resolve(['repo1', 'repo2', 'repo3']);
     }, 2000);
   });
 }
