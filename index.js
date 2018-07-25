5d90312f2d3127f7f96f37bea3aed5e9dfdf0c55
Nikola Pervic <nikolapervic@me.com>
2018-07-26 01:43:34 +0200

    Promise example
    Refactored the functions to use promises.

--- index.js
+++ index.js
@@ -1,44 +1,37 @@
-// Callback aproach
+// Promises aproach
 console.log('Before');
-getUser(1, getRepoitories);
+getUser(1)
+.then(user => getRepoitories(user.gitHubUsername))
+.then(repos => getCommits(repos[0]))
+.then(commits => console.log('Commits', commits))
+.catch(err => console.log('Error', err.message));
+
+function getUser(id) {
+  return new Promise ((resolve, reject) => {
+    // Start async work
+    setTimeout(() => {
+      console.log('Reading a user from the database...');
+      resolve({ id: id, gitHubUsername: 'nikola' });
+    }, 2000);
+  });
+  
+}
 console.log('After');
 
-function getRepoitories(user) {
-  getRepoitories(user.gitHubUsername. getCommits);
+function getCommits(repo) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Reading commits from the repository...');
+      resolve(['commit1', 'commit2']);
+    }, 2000);
+  });
 }
 
-function getCommits(repos) {
-  getCommits(repo, displayCommits);
-}
-
-function displayCommits(commits) {
-  console.log(commits)
-}
-
-function displayRepositories(repos) {
-  console.log(repos);
-}
-
-function getUser(id, callback) {
-  setTimeout(() => {
-    console.log('Reading a user from the database...');
-    callback({ id: id, gitHubUsername: 'nikola' });
-  }, 2000);
-}
-
-function getCommits(repo, callback) {
-  setTimeout(() => {
-    console.log('Reading commits from the repository...');
-    callback({ id: 22, commits: ['commit1', 'commit2'] });
-  }, 2000);
-}
-
-function getRepoitories(username, callback) {
-  setTimeout(() => {
-    console.log('Reading users repositories from github...');
-    callback({
-      username: username,
-      repositories: ['repo1', 'repo2', 'repo3']
-    });
-  }, 2000);
+function getRepoitories(username) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Reading users repositories from github...');
+      resolve(['repo1', 'repo2', 'repo3']);
+    }, 2000);
+  });
 }
